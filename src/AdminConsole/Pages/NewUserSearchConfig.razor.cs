c318e64f8fcce42f919906719a4c8dbd88e176ec
Timothy Small <smalls.online@icloud.com>
2023-03-23 12:53:27 -0400

    Add new user search config page

---
+++ src/AdminConsole/Pages/NewUserSearchConfig.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Config;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Services;
+
+namespace SmallsOnline.PasswordExpirationNotifier.AdminConsole.Pages;
+
+public partial class NewUserSearchConfig : ComponentBase
+{
+    [Inject]
+    protected ILogger<EmailTemplates> _logger { get; set; } = null!;
+
+    [Inject]
+    protected AuthenticationStateProvider _authenticationStateProvider { get; set; } = null!;
+
+    private UserSearchConfig? _userSearchConfig;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+        if (authState.User.Identity is not null && authState.User.Identity.IsAuthenticated)
+        {
+            _userSearchConfig = new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                PartitionKey = "user-search-config"
+            };
+        }
+        else
+        {
+            _logger.LogInformation("User is not authenticated.");
+        }
+    }
+}
