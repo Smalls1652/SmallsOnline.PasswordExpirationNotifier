967e0701423f4fa2c04489a89ef6143fa8bae878
Timothy Small <smalls.online@icloud.com>
2023-03-27 10:33:49 -0400

    Add JSON source generation to AdminConsole

---
+++ src/AdminConsole/JsonSourceGenerationContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Config;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Graph;
+
+namespace SmallsOnline.PasswordExpirationNotifier.AdminConsole;
+
+[JsonSourceGenerationOptions(
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    GenerationMode = JsonSourceGenerationMode.Default,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+)]
+[JsonSerializable(typeof(EmailAddress))]
+[JsonSerializable(typeof(FileAttachment))]
+[JsonSerializable(typeof(FileAttachment[]))]
+[JsonSerializable(typeof(MailMessage))]
+[JsonSerializable(typeof(Message))]
+[JsonSerializable(typeof(MessageBody))]
+[JsonSerializable(typeof(Recipient))]
+[JsonSerializable(typeof(Recipient[]))]
+[JsonSerializable(typeof(User))]
+[JsonSerializable(typeof(User[]))]
+[JsonSerializable(typeof(UserCollection))]
+[JsonSerializable(typeof(UserSearchQueueItem))]
+[JsonSerializable(typeof(UserSearchConfig))]
+[JsonSerializable(typeof(UserSearchConfig[]))]
+[JsonSerializable(typeof(UserPasswordExpirationDetails))]
+[JsonSerializable(typeof(UserPasswordExpirationDetails[]))]
+[JsonSerializable(typeof(EmailTemplateConfig))]
+[JsonSerializable(typeof(EmailTemplateConfig[]))]
+[JsonSerializable(typeof(EmailTemplateAttachmentItem))]
+[JsonSerializable(typeof(EmailTemplateAttachmentItem[]))]
+internal partial class JsonSourceGenerationContext : JsonSerializerContext
+{
+}
