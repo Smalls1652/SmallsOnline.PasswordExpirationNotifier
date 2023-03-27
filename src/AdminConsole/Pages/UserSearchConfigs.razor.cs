fcb1ac52fb49e0702e404655dfc786b62f87b67b
Timothy Small <smalls.online@icloud.com>
2023-03-27 10:41:09 -0400

    Add invoking user search config manually

--- src/AdminConsole/Pages/UserSearchConfigs.razor.cs
+++ src/AdminConsole/Pages/UserSearchConfigs.razor.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models;
 using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Config;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Graph;
 using SmallsOnline.PasswordExpirationNotifier.Lib.Services;
 
 namespace SmallsOnline.PasswordExpirationNotifier.AdminConsole.Pages;
@@ -17,6 +20,9 @@ public partial class UserSearchConfigs : ComponentBase
     [Inject]
     protected ICosmosDbClientService _cosmosDbClientService { get; set; } = null!;
 
+    [Inject]
+    protected IQueueClientService _queueClientService { get; set; } = null!;
+
     /// <summary>
     /// Logger for the page.
     /// </summary>
@@ -30,6 +36,7 @@ public partial class UserSearchConfigs : ComponentBase
     protected AuthenticationStateProvider _authenticationStateProvider { get; set; } = null!;
 
     private UserSearchConfig[]? _userSearchConfigs;
+    private JsonSourceGenerationContext _jsonSourceGenerationContext = new();
 
     private bool _loading = false;
 
@@ -54,10 +61,43 @@ public partial class UserSearchConfigs : ComponentBase
         _loading = false;
     }
 
+    private async Task HandleInvokeUserSearchConfigAsync(string id)
+    {
+        _logger.LogWarning("Invoking user search config with id: {id}", id);
+        UserSearchConfig configItem = await _cosmosDbClientService.GetUserSearchConfigAsync(id);
+
+        Range range = new(65, 90);
+        for (int i = range.Start.Value; i <= range.End.Value; i++)
+        {
+            string lastNameStartsWithChar = Convert.ToChar(i).ToString();
+            _logger.LogInformation("Creating queue message for config '{ConfigName} [{ConfigId}]' with last name starting with '{LastNameStartsWithChar}'.", configItem.ConfigName, configItem.Id, lastNameStartsWithChar);
+
+            // Create the queue message.
+            await _queueClientService.UserSearchQueueClient.SendMessageAsync(
+                messageText: JsonSerializer.Serialize(
+                    value: new UserSearchQueueItem()
+                    {
+                        UserSearchConfigId = configItem.Id,
+                        DomainName = configItem.DomainName!,
+                        LastNameStartsWith = lastNameStartsWithChar,
+                        OUPath = configItem.OUPath,
+                        MaxPasswordAge = configItem.MaxPasswordAge,
+                        IsEmailIntervalsEnabled = configItem.IsEmailIntervalsEnabled,
+                        EmailIntervalDays = configItem.EmailIntervalDays,
+                        EmailTemplateId = configItem.EmailTemplateId!
+                    },
+                    jsonTypeInfo: _jsonSourceGenerationContext.UserSearchQueueItem
+                )
+            );
+        }
+    }
+
     private async Task HandleRemoveUserSearchConfigAsync(string id)
     {
         _logger.LogWarning("Removing user search config with id: {id}", id);
         await _cosmosDbClientService.RemoveUserSearchConfigAsync(id);
         _userSearchConfigs = Array.FindAll(_userSearchConfigs!, item => item.Id != id);
     }
+
+
 }
