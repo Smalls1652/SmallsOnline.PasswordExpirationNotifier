644207514177f0e9364d73e0977b5a1a96484f6c
Timothy Small <smalls.online@icloud.com>
2023-03-27 15:42:01 -0400

    Add DeleteModal component

---
+++ src/AdminConsole/Shared/Common/DeleteModal.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace SmallsOnline.PasswordExpirationNotifier.AdminConsole.Shared.Common;
+
+public partial class DeleteModal : ComponentBase
+{
+    [Parameter]
+    public string? ModalId { get; set; }
+
+    [Parameter]
+    public string? ModalTitle { get; set; }
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = null!;
+
+    [Parameter]
+    public string? CallbackData { get; set; }
+
+    [Parameter]
+    public EventCallback<string> OnConfirmedCallback { get; set; }
+
+    private async Task HandleConfirmedAsync(MouseEventArgs eventArgs)
+    {
+        await OnConfirmedCallback.InvokeAsync(CallbackData);
+    }
+}
