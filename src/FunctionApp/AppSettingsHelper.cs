7c2da07b68c231e27daf3b1557f8ec61e8955b4c
Timothy Small <smalls.online@icloud.com>
2023-05-09 10:24:56 -0400

    Add XML code documentation to AppSettingsHelper

--- src/FunctionApp/AppSettingsHelper.cs
+++ src/FunctionApp/AppSettingsHelper.cs
@@ -1,7 +1,16 @@
 ﻿namespace SmallsOnline.PasswordExpirationNotifier.FunctionApp;
 
+/// <summary>
+/// Helper class for reading app settings from environment variables.
+/// </summary>
 public static class AppSettingsHelper
 {
+    /// <summary>
+    /// Get the value of an environment variable.
+    /// </summary>
+    /// <param name="settingName">The name of the environment variable the setting value is stored in.</param>
+    /// <returns>The setting value.</returns>
+    /// <exception cref="NullReferenceException">No environment variable matched the supplied setting name.</exception>
     public static string GetSettingValue(string settingName)
     {
         string? settingValue = Environment.GetEnvironmentVariable(settingName);
