8af58b2f42c34367b5fd69ad79a5eadfe10b2591
Timothy Small <smalls.online@icloud.com>
2023-05-15 12:26:44 -0400

    Add XML doc to UserEmailRedirectConfig

--- src/Lib/Models/Config/UserEmailRedirectConfig.cs
+++ src/Lib/Models/Config/UserEmailRedirectConfig.cs
@@ -2,17 +2,24 @@
 
 namespace SmallsOnline.PasswordExpirationNotifier.Lib.Models.Config;
 
+/// <summary>
+/// Holds config data for redirecting emails for one user to another.
+/// </summary>
 public class UserEmailRedirectConfig : IUserEmailRedirectConfig
 {
+    /// <inheritdoc />
     [JsonPropertyName("id")]
     public string Id { get; set; } = null!;
 
+    /// <inheritdoc />
     [JsonPropertyName("partitionKey")]
     public string PartitionKey { get; set; } = null!;
 
+    /// <inheritdoc />
     [JsonPropertyName("userPrincipalName")]
     public string? UserPrincipalName { get; set; }
 
+    /// <inheritdoc />
     [JsonPropertyName("redirectUserPrincipalName")]
     public string? RedirectUserPrincipalName { get; set; }
 }
