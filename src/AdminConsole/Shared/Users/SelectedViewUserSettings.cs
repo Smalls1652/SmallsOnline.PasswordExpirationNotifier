f35499d6520b852ab1e1727856d38c813e8d8fd6
Timothy Small <smalls.online@icloud.com>
2023-05-19 13:31:07 -0400

    Add SelectedViewUserSettings

---
+++ src/AdminConsole/Shared/Users/SelectedViewUserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Config;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Graph;
+
+namespace SmallsOnline.PasswordExpirationNotifier.AdminConsole.Shared.Users;
+
+public class SelectedViewUserSettings
+{
+    [Required]
+    public string? UserPrincipalName { get; set; }
+
+    [Required]
+    [StringLength(int.MaxValue, MinimumLength = 1)]
+    public string? SearchConfigId { get; set; }
+}
