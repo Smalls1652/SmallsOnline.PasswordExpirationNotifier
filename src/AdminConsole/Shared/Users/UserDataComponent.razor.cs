677da8e057f83aed989b00c8fa9bc29a916c625b
Timothy Small <smalls.online@icloud.com>
2023-05-19 13:30:53 -0400

    Add UserDataComponent

---
+++ src/AdminConsole/Shared/Users/UserDataComponent.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Config;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Graph;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Services;
+
+namespace SmallsOnline.PasswordExpirationNotifier.AdminConsole.Shared.Users;
+
+public partial class UserDataComponent : ComponentBase
+{
+    [Parameter]
+    public User UserData { get; set; } = null!;
+
+    [Parameter]
+    public UserSearchConfig UserSearchConfig { get; set; } = null!;
+
+    private bool _isLoaded;
+    private UserPasswordExpirationDetails? _userPasswordExpirationDetails;
+
+    protected override void OnParametersSet()
+    {
+        _isLoaded = false;
+
+        _userPasswordExpirationDetails = new(
+            user: UserData,
+            searchConfig: UserSearchConfig
+        );
+
+        _isLoaded = true;
+    }
+}
