12b8e9a00aed01069dcd05c3294621c015b1f415
Timothy Small <smalls.online@icloud.com>
2023-07-10 10:46:17 -0400

    Modify how DB data is deserialized
    Instead of relying on the Azure CosmosDB SDK for handling how JSON documents are deserialized into
    .NET types, we're now deserializing the JSON ourselves and
    taking advantage of source generated JSON.

--- src/Lib/Services/CosmosDBClientService/user-search-configs/GetUserSearchConfigsAsync.cs
+++ src/Lib/Services/CosmosDBClientService/user-search-configs/GetUserSearchConfigsAsync.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using System.Text.Json;
+using Microsoft.Azure.Cosmos;
+using SmallsOnline.PasswordExpirationNotifier.Lib.Models;
 using SmallsOnline.PasswordExpirationNotifier.Lib.Models.Config;
 
 namespace SmallsOnline.PasswordExpirationNotifier.Lib.Services;
@@ -14,20 +16,8 @@ public partial class CosmosDbClientService
             containerId: "configs"
         );
 
-        // Define the query to get the total number of configs.
-        QueryDefinition countQuery = new("SELECT VALUE COUNT(1) FROM c WHERE c.partitionKey = 'user-search-config'");
-
         // Get the total number of configs.
-        int totalConfigCount = 0;
-        FeedResponse<int> countQueryResponse = await container.GetItemQueryIterator<int>(
-                queryDefinition: countQuery,
-                requestOptions: new()
-                {
-                    MaxItemCount = 1
-                }
-            )
-            .ReadNextAsync();
-        totalConfigCount = countQueryResponse.FirstOrDefault();
+        int totalConfigCount = await GetTotalItemCountAsync("user-search-config");
 
         // If no configs were found, throw an exception.
         if (totalConfigCount == 0)
@@ -39,23 +29,33 @@ public partial class CosmosDbClientService
         UserSearchConfig[] configs = new UserSearchConfig[totalConfigCount];
 
         // Define the query to get the IDs of all configs.
-        QueryDefinition configIdsQuery = new("SELECT VALUE c.id FROM c WHERE c.partitionKey = 'user-search-config'");
+        QueryDefinition configsQuery = new("SELECT * FROM c");
 
-        // Get the IDs of all configs and get each config.
-        // Note: We're using a FeedIterator<string> here because we're only getting the IDs of the configs,
-        // which is then passed to GetEmailTemplateConfigAsync to get the full config. This is done to
-        // utilize source generated JSON deserialization when getting the data from Cosmos DB, since
-        // the Cosmos DB SDK doesn't support source generated JSON deserialization yet.
-        using FeedIterator<string> configsIterator = container.GetItemQueryIterator<string>(configIdsQuery);
-        while (configsIterator.HasMoreResults)
-        {
-            int i = 0;
-            foreach (var item in await configsIterator.ReadNextAsync())
+        using FeedIterator feedIterator = container.GetItemQueryStreamIterator(
+            queryDefinition: configsQuery,
+            requestOptions: new()
             {
-                configs[i] = await GetUserSearchConfigAsync(item);
+                PartitionKey = new("user-search-config")
+            }
+        );
+
+        int i = 0;
+        while (feedIterator.HasMoreResults)
+        {
+            using ResponseMessage response = await feedIterator.ReadNextAsync();
+            using StreamReader streamReader = new(response.Content);
+            CosmosDbResponse<UserSearchConfig>? configsResponse = await JsonSerializer.DeserializeAsync(
+                utf8Json: streamReader.BaseStream,
+                jsonTypeInfo: _jsonSourceGenerationContext.CosmosDbResponseUserSearchConfig
+            );
+
+            foreach (UserSearchConfig config in configsResponse!.Documents!)
+            {
+                configs[i] = config;
                 i++;
             }
         }
+
         return configs;
     }
 }
