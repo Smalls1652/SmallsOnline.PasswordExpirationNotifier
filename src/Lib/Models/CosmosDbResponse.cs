31349d50b4e45d0b1d25447b40424e46eaaaf5c8
Timothy Small <smalls.online@icloud.com>
2023-07-10 10:39:07 -0400

    Add CosmosDbResponse class

---
+++ src/Lib/Models/CosmosDbResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmallsOnline.PasswordExpirationNotifier.Lib.Models;
+
+public class CosmosDbResponse<T>
+{
+    [JsonPropertyName("Documents")]
+    public T[]? Documents { get; set; }
+
+    [JsonPropertyName("_count")]
+    public int Count { get; set; }
+}
